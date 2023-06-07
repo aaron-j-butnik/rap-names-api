eb47273f2cafcf24100e237221f7e7791589f3b0
Aaron Butnik <aaronjbutnik@gmail.com>
2023-06-06 20:20:08 -0700

    Fixed mongodb conneciton problem/bug.  Changed index.html to index.ejs, and added it to new 'views' directory.  Working on post method for server side api.

--- server.js
+++ server.js
@@ -2,41 +2,42 @@ const express = require("express");
 const cors = require("cors");
 const PORT = 3000;
 const bodyParser = require("body-parser");
-const { MongoClient } = require("mongodb");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
+app.use(cors());
+app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
 
 const connectionString = `mongodb+srv://aaronjbutnik:lindall9158@cluster0.6ycyxsr.mongodb.net/?retryWrites=true&w=majority`;
 
-const rappers = {
-  "21 savage": {
-    birthName: "Shéyaa Bin Abraham-Joseph",
-    age: 29,
-    birthLocation: "London, England",
-  },
-  "little dicky": {
-    birthName: "David Andrew Burd",
-    age: 35,
-    birthLocation: "Cheltenham Township, Pennsylvania",
-  },
-  unknown: {
-    birthName: "unknown",
-    age: 0,
-    birthLocation: "unknown",
-  },
-};
+// const rappers = {
+//   "21 savage": {
+//     birthName: "Shéyaa Bin Abraham-Joseph",
+//     age: 29,
+//     birthLocation: "London, England",
+//   },
+//   "little dicky": {
+//     birthName: "David Andrew Burd",
+//     age: 35,
+//     birthLocation: "Cheltenham Township, Pennsylvania",
+//   },
+//   unknown: {
+//     birthName: "unknown",
+//     age: 0,
+//     birthLocation: "unknown",
+//   },
+// };
 
-MongoClient.connection(connectionString)
+MongoClient.connect(connectionString)
   .then((client) => {
     console.log("Connected to Database");
     const db = client.db("rap-names");
     const rappersCollection = db.collection("rappers");
 
     app.get("/", (req, res) => {
-      db.collection("collection-one")
+      db.collection("rappers")
         .find()
         .toArray()
         .then((results) => {
@@ -45,18 +46,23 @@ MongoClient.connection(connectionString)
         .catch((error) => console.log(error));
     });
 
-    app.get("/api", (req, res) => {
-      res.json(rappers);
-    });
+    //     app.post("/api/names", (req, res) => {
+    //       rappersCollection
+    //         .insertOne(req.body)
+    //         .then((result) => {
+    //           res.redirect("/");
+    //         })
+    //         .catch((error) => console.log(error));
+    //     });
 
-    app.get("/api/:name", (req, res) => {
-      const rapperName = req.params.name.toLocaleLowerCase();
-      if (rappers[rapperName]) {
-        res.json(rappers[rapperName]);
-      } else {
-        res.json(rappers.unknown);
-      }
-    });
+    //  app.get("/api/:name", (req, res) => {
+    //    const rapperName = req.params.name.toLocaleLowerCase();
+    //    if (rappers[rapperName]) {
+    //      res.json(rappers[rapperName]);
+    //    } else {
+    //      res.json(rappers.unknown);
+    //    }
+    //  });
   })
   .catch((error) => console.error(error));
 
