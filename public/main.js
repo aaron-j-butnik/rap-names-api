5d3a25c56befd25dfc495a55a6ea51a6a4ff43d8
Aaron Butnik <aaronjbutnik@gmail.com>
2023-06-13 15:28:28 -0700

    Added an event listener for each like button. Added an async fetch route for adding a like in main.js.

--- public/main.js
+++ public/main.js
@@ -2,10 +2,34 @@ const deleteButton = document.querySelector("#delete-button");
 const deleteName = document.querySelector("#delete-name");
 const messageDiv = document.querySelector("#message");
 const trashCan = document.querySelectorAll("#trash-can");
+const likeButton = document.querySelectorAll("#likes");
+
+likeButton.forEach((element) => element.addEventListener("click", addLike));
 
 trashCan.forEach((element) =>
   element.addEventListener("click", trashCanDelete)
 );
+
+async function addLike() {
+  const sName = this.parentNode.childNodes[1].innerText;
+  const bName = this.parentNode.childNodes[3].innerText;
+  const tLikes = Number(this.parentNode.childNodes[5].innerText);
+  try {
+    const response = await fetch("/addLike", {
+      method: "put",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        stageNameS: sName,
+        birthNameS: bName,
+        likesS: tLikes,
+      }),
+    });
+    const data = response.json();
+    location.reload();
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 async function trashCanDelete() {
   const sName = this.parentNode.childNodes[1].innerText;
